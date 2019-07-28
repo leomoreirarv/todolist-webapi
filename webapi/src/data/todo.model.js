f78797564e4079bb1720df18db461e794a3a3c0a
Leonardo Melo Moreira <leomm_dsg@hotmail.com>
2019-07-28 10:56:23 +0100

    IniT

---
+++ webapi/src/data/todo.model.js
@@ -0,0 +1,9 @@
+export default class Todo {
+    constructor(title, description, author, deadline, order) {
+        this.title = title;
+        this.description = description;
+        this.author = author;
+        this.deadline = deadline;
+        this.order = order;
+    }
+}
